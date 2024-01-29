0a3fed62524a897261f620f220ef885eb1ba8a9d
Isabel Guimarães <156446502+isabel-guim@users.noreply.github>
2024-01-29 14:13:53 +0000

    Comentários comentários XLM aos métodos de somar e subtrair

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -11,9 +11,21 @@ namespace Calculos
     /// </summary>
     internal class Aritmetica
     {
+        /// <summary>
+        /// Operação soma
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Retorna a soma de dois numeros</returns>
         public int Somar(int x, int y)
         { return x + y; }
 
+        /// <summary>
+        /// Operação de subtração
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Retorna o resultado da subtração de dois numeros</returns>
         public int Subtrair (int x, int y)
         {
             return x - y;
