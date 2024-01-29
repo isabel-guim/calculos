0883fde478adc56256a3297f2dd67b6b162856b8
Isabel Guimarães <156446502+isabel-guim@users.noreply.github>
2024-01-29 21:13:43 +0000

    Adicionar o método ConverterDistancias()

--- Conversoes.cs
+++ Conversoes.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Calculos
 {
-    internal class Conversoes
+
+    /// <summary>
+    /// Implementa operações de conversão
+    /// </summary>
+    internal static class Conversoes
     {
+        /// <summary>
+        /// Tipo de conversão de temperatura a executar.
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+
+
+        /// <summary>
+        /// Conversão de temperaturas.
+        /// </summary>
+        /// <param name="conversao">A conversão a efetuar.</param>
+        /// <param name="temperatura">A temperatua a converter.</param>
+        /// <returns>Retorna o resultado da conversão da temperatura.</returns>
+        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }
+
+            return -1;
+        }
+        /// <summary>
+        /// Conversão de distâncias.
+        /// </summary>
+        /// <param name="conversao">A conversão a efetuar.</param>
+        /// <param name="distancia">A distância a converter</param>
+        /// <returns>Retorna o resultado da conversão da distância.</returns>
+        public static double ConverterDistancias(ConversaoDistancia conversao, double distancia)
+        {
+            if (conversao == ConversaoDistancia.MetrosMilhas)
+            {
+                return distancia * 0.0006213712;
+            }
+            else if (conversao == ConversaoDistancia.MilhasMetros)
+            {
+                return distancia * 1609.344;
+            }
+
+            return -1;
+        }
     }
 }
+
